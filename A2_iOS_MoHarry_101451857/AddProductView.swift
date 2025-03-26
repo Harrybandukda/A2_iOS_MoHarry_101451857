042c946e7ce3d1f69a7cf1ae158be6f22695e3b0
harrybandukda <harrisbandukda@gmail.com>
2025-03-26 16:08:35 -0700

    add product view added

--- A2_iOS_MoHarry_101451857/AddProductView.swift
+++ A2_iOS_MoHarry_101451857/AddProductView.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct AddProductView: View {
+    @ObservedObject var viewModel: ProductViewModel
+    @State private var name = ""
+    @State private var desc = ""
+    @State private var price = ""
+    @State private var provider = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                TextField("Product Name", text: $name)
+                TextField("Description", text: $desc)
+                TextField("Price", text: $price)
+                    .keyboardType(.decimalPad)
+                TextField("Provider", text: $provider)
+
+                Button("Add Product") {
+                    if let priceDouble = Double(price) {
+                        viewModel.addProduct(name: name, desc: desc, price: priceDouble, provider: provider)
+                    }
+                }
+            }
+            .navigationTitle("Add Product")
+        }
     }
 }
 
-#Preview {
-    AddProductView()
-}
+

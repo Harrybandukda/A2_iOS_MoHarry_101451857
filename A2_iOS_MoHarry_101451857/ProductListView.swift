1591770705ad2346f3e3d164126cad01aa7c9664
harrybandukda <harrisbandukda@gmail.com>
2025-03-26 17:59:29 -0700

    Improved the header in the main list view

--- A2_iOS_MoHarry_101451857/ProductListView.swift
+++ A2_iOS_MoHarry_101451857/ProductListView.swift
@@ -12,30 +12,36 @@ struct ProductListView: View {
 
     var body: some View {
         NavigationView {
-            List {
-                ForEach(viewModel.products) { product in
-                    NavigationLink(destination: ProductDetailView(product: product)) {
-                        VStack(alignment: .leading) {
-                            Text(product.name ?? "Unknown")
-                                .font(.headline)
-                            Text(product.desc ?? "No description")
-                                .font(.subheadline)
-                                .foregroundColor(.gray)
+            VStack {
+                Text("Product List")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding(.top, 10)
+
+                List {
+                    ForEach(viewModel.products) { product in
+                        NavigationLink(destination: ProductDetailView(product: product)) {
+                            VStack(alignment: .leading) {
+                                Text(product.name ?? "Unknown")
+                                    .font(.headline)
+                                Text(product.desc ?? "No description")
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
+                        }
+                    }
+                    .onDelete { indexSet in
+                        for index in indexSet {
+                            let productToDelete = viewModel.products[index]
+                            viewModel.deleteProduct(productToDelete)
                         }
                     }
                 }
-                .onDelete { indexSet in
-                    for index in indexSet {
-                        let productToDelete = viewModel.products[index]
-                        viewModel.deleteProduct(productToDelete)
-                    }
-                }
             }
-            .navigationTitle("Products")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
-
 
 #Preview {
     ProductListView()
